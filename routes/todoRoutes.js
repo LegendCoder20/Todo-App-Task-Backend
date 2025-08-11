9fe525ad79efe65bbf89516eacbfca0d3c1e7089
LegendCoder20 <20sit074.manjarekar.a20@gmail.com>
2025-08-11 13:19:49 +0530

    Fixed Few Bugs

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {protect} from "../middlewares/authMiddleware.js";
 import {
   getAllTodos,
+  getTodoDetail,
   createTodo,
   updateTodo,
   deleteTodo,
@@ -10,6 +11,7 @@ import {
 const router = express.Router();
 
 router.get("/todos", protect, getAllTodos);
+router.get("/todo/:id", protect, getTodoDetail);
 router.post("/createTodo", protect, createTodo);
 router.put("/todo/:id", protect, updateTodo);
 router.delete("/:id", protect, deleteTodo);
